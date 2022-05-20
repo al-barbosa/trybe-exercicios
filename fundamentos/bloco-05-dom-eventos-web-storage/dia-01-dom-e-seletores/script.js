e79791a61ef75ec40d0a458054d9ea0d93bb72b2
Alberto Barbosa <fbarbosa.alberto@gmail.com>
2022-05-20 14:16:14 -0300

    Exercícios dos dias 19/05/2022 e 20/05/2022

---
+++ fundamentos/bloco-05-dom-eventos-web-storage/dia-01-dom-e-seletores/script.js
@@ -0,0 +1,14 @@
+let word = document.getElementsByTagName('p');
+let main = document.getElementsByTagName('main');
+let h1 = document.getElementsByClassName('title')
+let center = document.getElementsByClassName('center-content')
+
+word[0].innerText = 'Me vejo trabalhando e feliz, em um Brasil melhor.';
+main[0].style.backgroundColor = 'rgb(76,164,109)'
+h1[0].innerText = "Exercício 5.1 - JavaScript"
+center[0].style.backgroundColor = '#FFF'
+console.log(center[0].parentElement)
+for (let i in word){
+  console.log(word[i].innerHTML)
+  word[i].style.textTransform = 'uppercase';
+}
