6e01e7fc7adc9ed7fef8bb84786be89d85a15467
Alberto Barbosa <fbarbosa.alberto@gmail.com>
2022-06-10 16:51:44 -0300

    10/06/2022

---
+++ fundamentos/bloco-07-es6-e-testes/dia-03-jest/script.test.js
@@ -0,0 +1,15 @@
+const {sum, sub} = require('./script');
+
+// describe('Sum function', () => {
+//   test('sums two values', () => {
+//     expect(sum(2, 3)).toBe(5);
+//   });
+// });
+
+// describe('Sub function', () => {
+//   test('sub two values', () => {
+//     expect(sub(5, 3)).toBe(2)
+//   })
+// })
+
+console.log(sub(5,3))
