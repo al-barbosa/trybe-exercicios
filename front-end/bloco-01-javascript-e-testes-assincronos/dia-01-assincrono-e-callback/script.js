0c1921710ec2b389d803b45fba21b5a2f8e79156
Alberto Barbosa <fbarbosa.alberto@gmail.com>
2022-07-06 19:33:14 -0300

    06/07/22

--- front-end/bloco-01-javascript-e-testes-assincronos/dia-01-assincrono-e-callback/script.js
+++ front-end/bloco-01-javascript-e-testes-assincronos/dia-01-assincrono-e-callback/script.js
@@ -1,95 +1,40 @@
-const despesas = [
+const pokemons = [
   {
-    gym: 99,
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
   },
   {
-    ifood: 200,
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
   },
   {
-    phone: 60,
-  },
-  {
-    internet: 100,
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
   },
 ];
 
-const renda = 1000;
-
-const despesaMensal = (renda, despesas, callback) => {
-
-  const despesaTotal = callback(despesas);
-  const saldoFinal = renda - despesaTotal;
-
-  console.log(`Balanço do mês:
-    Recebido: R$${renda},00
-    Gasto: R$${despesaTotal},00
-    Saldo: R$${saldoFinal},00 `);
-};
-
-const somaDespesas = (despesas) => {
-  const custoItem = despesas.map((item) => Object.values(item));
-  const despesaTotal = custoItem.reduce((acc, curr) => acc += curr[0], 0);
-  return despesaTotal;
-};
-
-// acc é a sigla para accumulator (acumulador)
-// curr é a sigla para current (valor atual)
-
-
-const resultadoFinal = (resultado) => {
-  console.log(resultado);
-}
-
-const funcSoma = (num1, num2, callback) => {
-  callback((num1+num2))
-}
-
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
-
-const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
-
-const getUser = (callback) => {
+function getPokemonDetails(filter, callback) {
   setTimeout(() => {
-    const user = {
-      firstName: "Ivan",
-      lastName: "Ivanovich",
-      nationality: "Russian",
-    };
-    // Retorne a `callback` passada como parâmetro na função `getUser`
-    // Dica: você pode manter o `console.log()`
-    console.log(callback(user))
-  }, delay());
-};
-
-// getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
-// getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
-
-const countryName = ({ name }) => console.log(`Returned country is ${name}`);
-const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
-
-const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
-
-const getCountry = (onSuccess, onFailure) => {
-  setTimeout(() => {
-    const didOperationSucceed = Math.random() >= 0.5;
-    if(didOperationSucceed) {
-      const country = {
-        name: "Brazil",
-        hdi: 0.759,
-        currency: "Real",
-      };
-      onSuccess(country);
-    } else {
-      const errorMessage = "Country could not be found";
-      onFailure(errorMessage)
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
     }
-  }, delay());
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+getPokemonDetails(
+  
+);
+
+module.exports = {
+  getPokemonDetails,
 };
-
-// Deve imprimir "Returned country is Brazil" no sucesso ou "Error getting country: Country could not be found" em caso de falha
-// getCountry(countryName, printErrorMessage);
-
-// // Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em caso de falha
-// getCountry(countryCurrency, printErrorMessage);
-
