c2d51f5c8f2eb77275807da9d40bb5dd478cb3ac
Alberto Barbosa <fbarbosa.alberto@gmail.com>
2022-07-18 19:53:13 -0300

    18/07/22

---
+++ front-end/bloco-02-intro-react/dia-01-hello-world/my-app/src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Header from './header.jsx';
+import Content from './content.jsx';
+import Footer from './footer.jsx'
+
+// const Task = (value) => {
+//   return (
+//     <li key={value}>{value}</li>
+//   )
+// }
+
+// const Arr = ['DataScience', 'Estatística', 'SQL']
+
+class App extends React.Component {
+  
+}
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Content />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ front-end/bloco-02-intro-react/dia-01-hello-world/my-app/src/content.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+class Content extends React.Component {
+
+  render() {
+
+    const conteudos = [
+      {
+        conteudo: 'High Order Functions',
+        bloco: 8,
+        status: 'Aprendido'
+      },
+      {
+        conteudo: 'Composicao de Componentes',
+        bloco: 11,
+        status: 'Aprendendo',
+      },
+      {
+        conteudo: 'Composicao de Estados',
+        bloco: 12,
+        status: 'Aprenderei'
+      },
+      {
+        conteudo: 'Redux',
+        bloco: 16,
+        status: 'Aprenderei'
+      },
+    ];
+
+    return (
+      <div>
+        <ul>{conteudos.map(materia => {
+          return (
+            <div key={materia.conteudo}>
+              <li>O conteúdo é: {materia.conteudo}</li>
+              <li>Status: {materia.status}</li>
+              <li>Bloco: {materia.bloco}</li>
+              <br></br>
+            </div>
+          )
+        })}</ul>
+      </div>
+    )
+  }
+}
+
+export default Content
